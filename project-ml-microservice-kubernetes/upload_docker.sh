39dc4856d3c67486c956d1414b053e14ea30aa1f
Uzukwu, John Paul <52359007+uzukwujp@users.noreply.github.com>
2022-07-27 16:57:34 +0100

    Update upload_docker.sh

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+# dockerpath=uzukwujp/udacity:v1
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag  uzukwujp/udacity:latest  uzukwujp/udacity:v1
+docker login -u uzukwujp -p $password
 # Step 3:
 # Push image to a docker repository
+docker push uzukwujp/udacity:v1
+
