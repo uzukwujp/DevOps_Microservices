2303f257a4df34d380022bc1c17db1f56614c69e
unknown <jchuks69@gmail.com>
2022-07-28 12:23:41 +0100

    fixed a bug

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,7 +8,7 @@
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run flask-app --image = uzukwujp/flaskapp:v7 --port = 80
+kubectl run flask-app --image=uzukwujp/flaskapp:v7 --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,7 +16,7 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-sleep 20
+sleep 100
 kubectl port-forward flask-app 8000:80
 
 
